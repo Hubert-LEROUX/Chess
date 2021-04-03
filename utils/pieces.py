8489cfebceb18b47555bccd56df35379aab52b14
Hubert-LEROUX <hubertleroux1@gmail.com>
2021-04-03 12:09:29 +0200

    Change in utils structure

---
+++ utils/pieces.py
@@ -0,0 +1,19 @@
+
+
+class Piece():
+    def __init__(self):
+        pass
+
+
+class King(Piece):
+    def __init__(self):
+        super().__init__()
+class Queen(Piece):
+    def __init__(self):
+        super().__init__()
+class Pawn(Piece):
+    def __init__(self):
+        super().__init__()
+class Tower(Piece):
+    def __init__(self):
+        super().__init__()
